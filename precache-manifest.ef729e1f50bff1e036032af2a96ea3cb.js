bdf66ea2bec2c8d8ac0ad7645af2929f560f7c6b
Евгений <evgenij.romanko@neoseo.com.ua>
2020-07-29 22:27:56 +0300

    Updates

---
+++ precache-manifest.ef729e1f50bff1e036032af2a96ea3cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77a75b5f70cbf9392032a707f1bcb47f",
+    "url": "/test-test/index.html"
+  },
+  {
+    "revision": "ff4efbc8b18fda74ecb1",
+    "url": "/test-test/static/css/2.972ec59c.chunk.css"
+  },
+  {
+    "revision": "6a24d8807c31fd206453",
+    "url": "/test-test/static/css/main.f0df6e70.chunk.css"
+  },
+  {
+    "revision": "ff4efbc8b18fda74ecb1",
+    "url": "/test-test/static/js/2.76fc501b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-test/static/js/2.76fc501b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a24d8807c31fd206453",
+    "url": "/test-test/static/js/main.b70a8ff6.chunk.js"
+  },
+  {
+    "revision": "b3403e6b4475c58755e3",
+    "url": "/test-test/static/js/runtime-main.ae8ce451.js"
+  }
+]);
